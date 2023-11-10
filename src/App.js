9611694c2a388b792408ce5f0f1e765d2f8425ef
THeAndrew04 <144220882+THeAndrew04@users.noreply.github.com>
2023-11-11 02:30:15 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MainField from './components/MainField';
+
+
+function App() {
+  return (
+    <div className='App-header'>
+    <MainField/>
+    
+    </div>
+  );
+}
+
+export default App;
